aae519daa587f4a46f1adeb4a52516f042d1bd60
huttatran <hdtran114@student.hbuhsd.edu>
2025-10-27 21:46:51 +0000

    puh

--- Main.java
+++ Main.java
@@ -4,27 +4,67 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    // printNTimes("code", 3);
+    // printNums();
+    // uprightNumberTriangle(5);
+    starTree();
+    multTable();
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    int i = 0;
+    String output = "";
+    while (i < word.length()) {
+      String n = word.substring(i, i + 1);
+      output = output + n + n + n;
+      i++;
+    }
+    System.out.println(output);
   }
 
   public static void printNums()
   {
-    // code solution here
+    int i = 10;
+    while (i > 0) {
+      for (int j = 0; j < i; j++) {
+        System.out.print(i + " ");
+      }
+      System.out.println("");
+      i--;
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    int i = 1;
+    while (i <= N) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j + " ");
+      }
+      System.out.println("");
+      i++;
+    }
   }
 
   public static void starTree()
   {
-    // code solution here
+    int i = 10;
+    while (i > 0) {
+      int l = 0;
+      int e = 0;
+      for (int j = 0; j < i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println("");
+      
+      while (l < e) {
+        System.out.print(" ");
+        l++;
+      }
+      e++;
+      i--;
+    }
   }
 
   public static void multTable()
